2425cf23fa0938006a2b08ee0f6aaf0c4d04bb59
Kazzer <kazzer.nd@gmail.com>
2022-07-29 16:47:03 +0300

    Initial Commit

---
+++ pulse animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pulse animation
+//
+//  Created by Eugene Ned on 29.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pulse animation/pulse_animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  pulse_animationApp.swift
+//  pulse animation
+//
+//  Created by Eugene Ned on 29.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct pulse_animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
